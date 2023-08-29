4cd258716b8fc3e1e1ea26cba57f50cf220fe4d4
Andy-Fernandez <58851171+Andy-Fernandez@users.noreply.github.com>
2023-08-29 09:32:47 -0400

    Mis cambios

--- Proyecto2023/vista/cargoLista.php
+++ Proyecto2023/vista/cargoLista.php
@@ -1,9 +1,21 @@
+<?php
+// Realizar la conexión a la base de datos
+$conn = mysqli_connect("localhost", "root", "28281010", "test2");
+
+// Verificar la conexión
+if (!$conn) {
+    die("La conexión falló: " . mysqli_connect_error());
+}
+
+// Realizar una consulta a la base de datos
+$query = "SELECT id_cargo, cargo FROM tabla_de_cargos";
+$res = mysqli_query($conn, $query);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Document</title>
+<!-- ... metadatos y título ... -->
 </head>
 <body>
 <table>
@@ -12,15 +24,17 @@
 <td>CARGO</td>
 </tr>
 <?php
-
+// Utilizar los resultados obtenidos en la consulta
 while ($reg = mysqli_fetch_array($res)) {
     echo "<tr>";
     echo "<td>" . $reg['id_cargo'] . "</td>";
     echo "<td>" . $reg['cargo'] . "</td>";
     echo "<tr>";
 }
+
+// Cerrar la conexión
+mysqli_close($conn);
 ?>
 </table>
-
 </body>
 </html>
